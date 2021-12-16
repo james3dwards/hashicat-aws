8eee9c949eb601115504af72cb81b39ac7aa241b
jamese <james.edwards@insight.com>
2021-12-16 23:23:38 +0000

    updated adding s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/JDE/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "JDE20221234"
+  bucket_prefix = "jde2021hashicat"
 }
